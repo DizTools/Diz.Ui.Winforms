1f7336026a9c35b1bfba978bfe0cdebcece5af9d
Dominic Cerquetti <binary1230@users.noreply.github.com>
2022-01-16 12:27:52 -0500

    add ui-specific service container - this allows directly using winforms-based serivces - change fromework type to winforms (ok since this is a winforms-specific project)

---
+++ Diz.Ui.Winforms.Tests/Diz.Ui.Winforms.Test/ContainerUiFixture.cs
@@ -0,0 +1,17 @@
+﻿using Diz.Test.Utils;
+using DiztinGUIsh;
+using LightInject;
+
+namespace Diz.Ui.Winforms.Test;
+
+public class ContainerUiFixture : ContainerFixture
+{
+    protected override void Configure(IServiceRegistry serviceRegistry)
+    {
+        // not going to call the base, we're going to start even higher level than this.
+        // our function call should eventually register the same services as what the base does
+        // base.Configure(serviceRegistry);
+
+        DizAppServices.RegisterDizUiServices(serviceRegistry);
+    }
+}
