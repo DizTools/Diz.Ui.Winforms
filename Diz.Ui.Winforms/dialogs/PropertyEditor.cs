5df400b1e4726e314d3cc91a69d1f653a2c719e1
Dominic Cerquetti <binary1230@users.noreply.github.com>
2023-12-17 16:45:44 -0500

    UI: add generic property grid editor - it'll display anything inside it in a nice grid format

---
+++ Diz.Ui.Winforms/dialogs/PropertyEditor.cs
@@ -0,0 +1,54 @@
+﻿namespace Diz.Ui.Winforms.dialogs;
+
+public sealed class GenericPropertyEditorForm : Form
+{
+    private PropertyGrid propertyGrid;
+
+    public GenericPropertyEditorForm(object items, string windowTitle = "Edit Properties")
+    {
+        Width = 800;
+        Height = 600;
+        Text = windowTitle;
+        
+        propertyGrid = new PropertyGrid
+        {
+            Dock = DockStyle.Fill,
+            SelectedObject = items,
+        };
+        
+        Controls.Add(propertyGrid);
+        
+        Load += Form_Load;
+        Shown += Form_Shown;
+    }
+
+    private void Form_Load(object? sender, EventArgs e) { }
+    private void Form_Shown(object? sender, EventArgs e) {
+        ExpandAllGridItems();
+    }
+    
+    private void ExpandAllGridItems()
+    {
+        var root = propertyGrid.SelectedGridItem;
+        while (root.Parent != null) {
+            root = root.Parent;
+        }
+
+        ExpandAllGridItems(root);
+    }
+
+    private static void ExpandAllGridItems(GridItem? gridItem)
+    {
+        if (gridItem == null)
+            return;
+
+        foreach (GridItem item in gridItem.GridItems)
+        {
+            if (item.GridItemType != GridItemType.Category && !item.Expandable) 
+                continue;
+            
+            item.Expanded = true;
+            ExpandAllGridItems(item);
+        }
+    }
+}
