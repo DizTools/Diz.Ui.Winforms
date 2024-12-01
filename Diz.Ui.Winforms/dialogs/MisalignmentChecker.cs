3e4d6d3f6226f5c66c9caf3d992911d1803765d8
Dominic Cerquetti <binary1230@users.noreply.github.com>
2024-12-01 11:41:44 -0500

    misalignment checker: move into winforms UI project - no changes

---
+++ Diz.Ui.Winforms/dialogs/MisalignmentChecker.cs
@@ -0,0 +1,31 @@
+﻿using Diz.Core.model.snes;
+using Diz.Cpu._65816;
+
+namespace Diz.Ui.Winforms.dialogs;
+
+public partial class MisalignmentChecker : Form
+{
+    private Data Data { get; set; }
+    public MisalignmentChecker(Data data)
+    {
+        Data = data;
+        InitializeComponent();
+    }
+
+    private void cancel_Click(object sender, EventArgs e) => Close();
+
+    private void buttonScan_Click(object sender, EventArgs e)
+    {
+        var snesData = Data.GetSnesApi();
+        if (snesData == null)
+            return;
+
+        var (numFound, outputTextLog) = snesData.GenerateMisalignmentReport();
+        textLog.Text = outputTextLog;
+    }
+
+    private void buttonFix_Click(object sender, EventArgs e)
+    {
+        DialogResult = DialogResult.OK;
+    }
+}
