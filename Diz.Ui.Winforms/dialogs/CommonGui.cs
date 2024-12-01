843c08e1908b4968a25a499bf07aef7b8621b914
Dominic Cerquetti <binary1230@users.noreply.github.com>
2024-12-01 13:10:32 -0500

    commongui: move to winforms submodule

---
+++ Diz.Ui.Winforms/dialogs/CommonGui.cs
@@ -0,0 +1,19 @@
+﻿using Diz.Controllers.interfaces;
+using Diz.Ui.Winforms.util;
+
+namespace Diz.Ui.Winforms.dialogs;
+
+public class CommonGui : ICommonGui
+{
+    public bool PromptToConfirmAction(string msg) => 
+        GuiUtil.PromptToConfirmAction("Warning", msg, () => true);
+
+    public void ShowError(string msg) => 
+        MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    
+    public void ShowWarning(string msg) => 
+        MessageBox.Show(msg, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    
+    public void ShowMessage(string msg) => 
+        MessageBox.Show(msg, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+}
