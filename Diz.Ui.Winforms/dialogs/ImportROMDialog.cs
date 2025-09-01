9a5de79718c5db9990e1c467d471f295443fd697
Dominic Cerquetti <binary1230@users.noreply.github.com>
2025-08-31 23:53:53 -0400

    rom detection: add override for Contra3 settings - busted ROM SNES header requires us to have some custom detection code. sigh.

--- Diz.Ui.Winforms/dialogs/ImportROMDialog.cs
+++ Diz.Ui.Winforms/dialogs/ImportROMDialog.cs
@@ -134,6 +134,9 @@ public partial class ImportRomDialog : Form, IImportRomDialogView
 
     private void SetDefaultsIfDetectionFailed()
     {
+        // TODO: we need a way to update the UI when detection fails and a mode is manually selected.
+        //  currently, there's no way to manually override. the OK button stays greyed out forever.
+        
         romspeed.Text = "????";
         romtitle.Text = "?????????????????????";
         
