8d71e06b5c9573c8c67f0db464136c8b5fe17e4e
Dominic Cerquetti <binary1230@users.noreply.github.com>
2026-03-12 15:28:30 -0400

    UI: fix "goto next" UI column bug - harmless but certain operations would change the column you were on for no reason - these "1"'s were likely always supposed to be -1's to mean "stay on the current column"

--- Diz.Ui.Winforms/window/MainWindow.Actions.cs
+++ Diz.Ui.Winforms/window/MainWindow.Actions.cs
@@ -194,7 +194,7 @@ public partial class MainWindow
         if (snesOffset == -1)
             return;
             
-        SelectOffset(snesOffset, 1, new ISnesNavigation.HistoryArgs {Description = "GoTo Intermediate Addr"});
+        SelectOffset(snesOffset, -1, new ISnesNavigation.HistoryArgs {Description = "GoTo Intermediate Addr"}, overshootAmount: standardOvershootAmount);
     }
 
     public void GoTo(int offset)
@@ -215,7 +215,7 @@ public partial class MainWindow
         if (!FindUnreached(SelectedOffset, fromStartOrEnd, forwardDirection, out var unreached))
             return;
 
-        SelectOffset(unreached, 1, BuildUnreachedHistoryArgs(fromStartOrEnd, forwardDirection));
+        SelectOffset(unreached, -1, BuildUnreachedHistoryArgs(fromStartOrEnd, forwardDirection), overshootAmount: standardOvershootAmount);
     }
     
     private void GoToNextUnreachedBranchPoint(int offset)
